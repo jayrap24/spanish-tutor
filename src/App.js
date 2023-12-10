5954870057c7f9479e914838480867ace6f2c289
Jesper Rapouw <rapouw@gmail.com>
2023-12-10 14:48:27 -0500

    my first spanish commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { Button, Box, TextField } from '@mui/material';
 
 function App() {
+  const [answer, setAnswer] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box className="App">
+      <TextField
+        label="What do you want to learn"
+        onChange={(e) => setAnswer(e.target.value)}
+      />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => {
+          alert(answer);
+        }}
+      >
+        Generate
+      </Button>
+    </Box>
   );
 }
 
